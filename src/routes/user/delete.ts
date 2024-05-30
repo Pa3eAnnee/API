9d2cf005aecc7b27d385995034cf4280e0d6715a
vanande <vkhatchatrian1@myges.fr>
2024-05-30 23:29:42 +0200

    add route for user

---
+++ src/routes/user/delete.ts
@@ -0,0 +1,30 @@
+import { Express, Request, Response } from 'express';
+import { AppDataSource } from '../../database/database';
+import { User } from '../../database/entities/User';
+
+export const deleteUser = (app: Express): void => {
+    app.delete('/users/:id', async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id);
+
+        if (isNaN(userId)) {
+            res.status(400).send({ error: 'Invalid user ID' });
+            return;
+        }
+
+        const userRepo = AppDataSource.getRepository(User);
+
+        try {
+            const user = await userRepo.findOneBy({ id: userId });
+            if (!user) {
+                res.status(404).send({ error: `User with ID ${userId} not found` });
+                return;
+            }
+
+            await userRepo.delete(userId);
+            res.status(200).send({ message: `User with ID ${userId} deleted successfully` });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ error: 'Internal error' });
+        }
+    });
+}
