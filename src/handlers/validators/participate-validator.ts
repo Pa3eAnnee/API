90160d22ebaee004e25255d99903d9d35adc01f0
vanande <vkhatchatrian1@myges.fr>
2024-06-06 22:16:43 +0100

    add participate crud

---
+++ src/handlers/validators/participate-validator.ts
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+export interface CreateParticipateRequest {
+    user_id: number;
+    meeting_id: number;
+}
+
+export interface UpdateParticipateRequest extends CreateParticipateRequest {
+    id: number;
+}
+
+export const createParticipateValidation = Joi.object<CreateParticipateRequest>({
+    user_id: Joi.number().required(),
+    meeting_id: Joi.number().required(),
+}).options({ abortEarly: false });
+
+export const updateParticipateValidation = Joi.object<UpdateParticipateRequest>({
+    id: Joi.number().required(),
+    user_id: Joi.number().required(),
+    meeting_id: Joi.number().required(),
+}).options({ abortEarly: false });
