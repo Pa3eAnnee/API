577b1e263d4d2a980e87d14c67bb56e77a11b73d
vanande <vkhatchatrian1@myges.fr>
2024-06-08 23:08:23 +0100

    add event handling entities

---
+++ src/database/entities/Furniture.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "Furniture" })
+export class Furniture {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    category_id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    storage: string
+
+    constructor(
+        id: number,
+        category_id: number,
+        name: string,
+        storage: string
+    ) {
+        this.id = id;
+        this.category_id = category_id;
+        this.name = name;
+        this.storage = storage;
+    }
+}
---
+++ src/database/entities/Rent.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "Rent" })
+export class Rent {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    furniture_id: number
+
+    @Column()
+    event_id: number
+
+    @Column()
+    cost: number
+
+    @Column()
+    quantity: number
+
+    constructor(
+        id: number,
+        furniture_id: number,
+        event_id: number,
+        cost: number,
+        quantity: number
+    ) {
+        this.id = id;
+        this.furniture_id = furniture_id;
+        this.event_id = event_id;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
+}
