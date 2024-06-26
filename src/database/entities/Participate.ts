536fed8880b3202449633224130785e591ebe7f4
v.khatchatrian <v.khatchatrian@groupeonepoint.com>
2024-06-26 17:21:29 +0200

    update participate

--- src/database/entities/Participate.ts
+++ src/database/entities/Participate.ts
@@ -2,18 +2,26 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: "Participate" })
 export class Participate {
-	@PrimaryGeneratedColumn()
-	id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-	@Column()
-	user_id: number;
+    @Column()
+    user_id: number;
 
-	@Column()
-	meeting_id: number;
+    @Column()
+    activity_id: number;
 
-	constructor(id: number, user_id: number, meeting_id: number) {
-		this.id = id;
-		this.user_id = user_id;
-		this.meeting_id = meeting_id;
-	}
+    @Column()
+    role: string;
+
+    @Column()
+    present: boolean;
+
+    constructor(id: number, user_id: number, activity_id: number, role: string, present: boolean) {
+        this.id = id;
+        this.user_id = user_id;
+        this.activity_id = activity_id;
+        this.role = role;
+        this.present = present;
+    }
 }
--- src/handlers/validators/participate-validator.ts
+++ src/handlers/validators/participate-validator.ts
@@ -1,25 +1,31 @@
 import Joi from "joi";
 
 export interface CreateParticipateRequest {
-	user_id: number;
-	meeting_id: number;
+    user_id: number;
+    activity_id: number;
+    role: string;
+    present: boolean;
 }
 
-export interface UpdateParticipateRequest extends CreateParticipateRequest {
-	id: number;
+export interface UpdateParticipateRequest {
+    id: number;
+    user_id?: number;
+    activity_id?: number;
+    role?: string;
+    present?: boolean;
 }
 
-export const createParticipateValidation = Joi.object<CreateParticipateRequest>(
-	{
-		user_id: Joi.number().required(),
-		meeting_id: Joi.number().required(),
-	},
-).options({ abortEarly: false });
+export const createParticipateValidation = Joi.object<CreateParticipateRequest>({
+    user_id: Joi.number().required(),
+    activity_id: Joi.number().required(),
+    role: Joi.string().required(),
+    present: Joi.boolean().required(),
+}).options({ abortEarly: false });
 
-export const updateParticipateValidation = Joi.object<UpdateParticipateRequest>(
-	{
-		id: Joi.number().required(),
-		user_id: Joi.number().required(),
-		meeting_id: Joi.number().required(),
-	},
-).options({ abortEarly: false });
+export const updateParticipateValidation = Joi.object<UpdateParticipateRequest>({
+    id: Joi.number().required(),
+    user_id: Joi.number().optional(),
+    activity_id: Joi.number().optional(),
+    role: Joi.string().optional(),
+    present: Joi.boolean().optional(),
+}).options({ abortEarly: false });
